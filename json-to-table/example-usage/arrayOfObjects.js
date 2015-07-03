228057f697b0113b9ba52fbce5dab5e2a9f3f1e8
Eric Lee Carraway <github@ericcarraway.com>
2015-07-03 17:53:56 -0500

    rename folders

---
+++ json-to-table/example-usage/arrayOfObjects.js
@@ -0,0 +1,152 @@
+var arrayOfObjects = [
+  {
+    "inventoryNumber": 1,
+    "title": "Maus",
+    "publisher": "Raw/Pantheon",
+    "writer": "A. Spiegelman"
+  },
+  {
+    "inventoryNumber": 2,
+    "title": "Watchmen",
+    "publisher": "DC/Vertigo",
+    "writer": "A. Moore"
+  },
+  {
+    "inventoryNumber": 3,
+    "title": "Sandman",
+    "publisher": "DC/Vertigo",
+    "writer": "N. Gaiman"
+  },
+  {
+    "inventoryNumber": 4,
+    "title": "The Dark Knight Returns",
+    "publisher": "DC",
+    "writer": "F. Miller"
+  },
+  {
+    "inventoryNumber": 5,
+    "title": "Preacher",
+    "publisher": "DC/Vertigo",
+    "writer": "G. Ennis"
+  },
+  {
+    "inventoryNumber": 6,
+    "title": "Transmetropolitan",
+    "publisher": "DC/Vertigo/Helix",
+    "writer": "W. Ellis"
+  },
+  {
+    "inventoryNumber": 7,
+    "title": "V for Vendetta",
+    "publisher": "Quality/DC/Vertigo",
+    "writer": "A. Moore"
+  },
+  {
+    "inventoryNumber": 8,
+    "title": "Ghost World",
+    "publisher": "Fantagraphics",
+    "writer": "D. Clowes"
+  },
+  {
+    "inventoryNumber": 9,
+    "title": "American Splendor",
+    "publisher": "Dark Horse/DC",
+    "writer": "H. Pekar"
+  },
+  {
+    "inventoryNumber": 10,
+    "title": "Hellblazer",
+    "publisher": "DC/Vertigo",
+    "writer": "Various"
+  },
+  {
+    "inventoryNumber": 11,
+    "title": "Marvels",
+    "publisher": "Marvel",
+    "writer": "K. Busiek"
+  },
+  {
+    "inventoryNumber": 12,
+    "title": "Essex County",
+    "publisher": "Top Shelf",
+    "writer": "J. Lemire"
+  },
+  {
+    "inventoryNumber": 13,
+    "title": "Swamp Thing",
+    "publisher": "DC/Vertigo",
+    "writer": "A. Moore"
+  },
+  {
+    "inventoryNumber": 14,
+    "title": "X-Men: God Loves, Man Kills",
+    "publisher": "Marvel",
+    "writer": "C. Claremont"
+  },
+  {
+    "inventoryNumber": 15,
+    "title": "All-Star Superman",
+    "publisher": "DC",
+    "writer": "G. Morrison"
+  },
+  {
+    "inventoryNumber": 16,
+    "title": "Sin City",
+    "publisher": "Dark Horse",
+    "writer": "F. Miller"
+  },
+  {
+    "inventoryNumber": 17,
+    "title": "Daytripper",
+    "publisher": "DC/Vertigo",
+    "writer": "F. Moon & G. Bá"
+  },
+  {
+    "inventoryNumber": 18,
+    "title": "The Killing Joke",
+    "publisher": "DC",
+    "writer": "A. Moore"
+  },
+  {
+    "inventoryNumber": 19,
+    "title": "Bone",
+    "publisher": "Cartoon/Image/Scholastic",
+    "writer": "J. Smith"
+  },
+  {
+    "inventoryNumber": 20,
+    "title": "Y: The Last Man",
+    "publisher": "DC/Vertigo",
+    "writer": "B. K. Vaughan"
+  },
+  {
+    "inventoryNumber": 21,
+    "title": "Fables",
+    "publisher": "DC/Vertigo",
+    "writer": "B. Willingham"
+  },
+  {
+    "inventoryNumber": 22,
+    "title": "Daredevil: Born Again",
+    "publisher": "Marvel",
+    "writer": "F. Miller"
+  },
+  {
+    "inventoryNumber": 23,
+    "title": "The Invisibles",
+    "publisher": "DC/Vertigo",
+    "writer": "G. Morrison"
+  },
+  {
+    "inventoryNumber": 24,
+    "title": "I Killed Adolf Hitler",
+    "publisher": "Fantagraphics",
+    "writer": "Jason"
+  },
+  {
+    "inventoryNumber": 25,
+    "title": "The Punisher MAX",
+    "publisher": "Marvel",
+    "writer": "G. Ennis"
+  }
+];
